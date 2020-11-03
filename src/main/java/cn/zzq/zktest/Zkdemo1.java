faaee48d754086d88c1a33de29124209a25e1777
周志钦 <zhiqin.zhou@ucarinc.com>
2020-11-03 15:41:27 +0800

    连接云服务器

--- src/main/java/cn/zzq/zktest/Zkdemo1.java
+++ src/main/java/cn/zzq/zktest/Zkdemo1.java
@@ -5,7 +5,7 @@ import org.apache.zookeeper.*;
 public class Zkdemo1 {
     public static void main(String[] args) throws Exception {
         ZooKeeper zk = new ZooKeeper(
-                "192.168.14.128:2181,192.168.14.129:2181,192.168.14.130:2181", 3000, new Watcher() {
+                "39.100.9.42:2181,39.100.9.42:2182,39.100.9.42:2183", 3000, new Watcher() {
             public void process(WatchedEvent event) {
                 System.out.println("事件类型：" + event.getType());
                 System.out.println("事件发生路径：" + event.getPath());
@@ -13,26 +13,13 @@ public class Zkdemo1 {
             }
         });
         // 创建一个目录节点
-        zk.create("/testRootPath", "testRootData".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
+        zk.create("/zzq2", "zkstudy".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
                 CreateMode.PERSISTENT);
-// 创建一个子目录节点
-        zk.create("/testRootPath/testChildPathOne", "testChildDataOne".getBytes(),
+        // 创建一个子目录节点
+        zk.create("/zzq2/rpc", "rpcstudy".getBytes(),
                 ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        System.out.println(new String(zk.getData("/testRootPath", false, null)));
-// 取出子目录节点列表
-        System.out.println(zk.getChildren("/testRootPath", true));
-// 修改子目录节点数据
-        zk.setData("/testRootPath/testChildPathOne", "modifyChildDataOne".getBytes(), -1);
-        System.out.println("目录节点状态：[" + zk.exists("/testRootPath", true) + "]");
-// 创建另外一个子目录节点
-        zk.create("/testRootPath/testChildPathTwo", "testChildDataTwo".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-        System.out.println(new String(zk.getData("/testRootPath/testChildPathTwo", true, null)));
-// 删除子目录节点
-        zk.delete("/testRootPath/testChildPathTwo", -1);
-        zk.delete("/testRootPath/testChildPathOne", -1);
-// 删除父目录节点
-        zk.delete("/testRootPath", -1);
+        System.out.println(zk.getChildren("/zzq2",null));
+        System.out.println((new String(zk.getData("/zzq2/rpc",true,null))));
         zk.close();
     }
 }
